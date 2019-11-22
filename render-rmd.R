b0cc0bf6de5039d7a273eb94500d9baa0ac13bee
Ruocheg Dong <RuochengDong@users.noreply.github.com>
2019-11-22 11:17:36 -0600

    how to rename

--- render-rmd.R
+++ render-rmd.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
-
+library(stringr)
 # render("surveys-report.Rmd")
 # Or you can type Rscript render-rmd.R in rstudio terminal
 
@@ -12,5 +12,23 @@ library(rmarkdown)
 
 # render("surveys-report.Rmd", output_dir = "reports", params = list(file = "data/plot1.csv"))
 
+# file_names = commandArgs(trailingOnly = TRUE)
+# render("surveys-report.Rmd", output_dir = "reports", params = list(file = file_names))
+# This need you to specify the file name you want to use
+# Rscript render-rmd.R data/complete_surveys.csv
+
+# file_names = commandArgs(trailingOnly = TRUE)
+# for (file in file_names) {
+#   render("surveys-report.Rmd", output_dir = "reports", params = list(file = file))
+# }
+# Rscript render-rmd.R data/plot1.csv data/plot2.csv
+
+
 file_names = commandArgs(trailingOnly = TRUE)
-render("surveys-report.Rmd", output_dir = "reports", params = list(file = file_names))
+for (file in file_names) {
+  out_name <- str_split_fixed(string = file, pattern = "[/|.]", n =3)
+  out_name <- str_c(out_name[2], ".html")
+  render("surveys-report.Rmd", output_file = out_name, output_dir = "reports", params = list(file = file))
+}
+
+
