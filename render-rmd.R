88e4c9c27376baad047a2e95cbeac41dfcec1796
Ruocheg Dong <RuochengDong@users.noreply.github.com>
2019-11-22 10:51:09 -0600

    Using extra parameters

--- render-rmd.R
+++ render-rmd.R
@@ -1,4 +1,13 @@
 library(rmarkdown)
 
-render("surveys-report.Rmd")
-# Or you can type Rscript render-rmd.R
+# render("surveys-report.Rmd")
+# Or you can type Rscript render-rmd.R in rstudio terminal
+
+# Git basics, in case I forget
+# git add
+# git commit -m "message"
+# git push
+
+# render("surveys-report.Rmd", output_dir = "reports")
+
+render("surveys-report.Rmd", output_dir = "reports", params = list(file = "data/plot1.csv"))
