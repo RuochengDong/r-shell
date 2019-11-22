247b8b18a1d34748984be674f2a89250ca38b1fc
Ruocheg Dong <RuochengDong@users.noreply.github.com>
2019-11-22 11:46:11 -0600

    use if, for

--- render-rmd.R
+++ render-rmd.R
@@ -3,10 +3,11 @@ library(stringr)
 # render("surveys-report.Rmd")
 # Or you can type Rscript render-rmd.R in rstudio terminal
 
-# Git basics, in case I forget
+# Git basics, shell basics, in case I forget
 # git add
 # git commit -m "message"
 # git push
+# *.csv to represent all csv file
 
 # render("surveys-report.Rmd", output_dir = "reports")
 
@@ -24,11 +25,49 @@ library(stringr)
 # Rscript render-rmd.R data/plot1.csv data/plot2.csv
 
 
-file_names = commandArgs(trailingOnly = TRUE)
-for (file in file_names) {
-  out_name <- str_split_fixed(string = file, pattern = "[/|.]", n =3)
-  out_name <- str_c(out_name[2], ".html")
-  render("surveys-report.Rmd", output_file = out_name, output_dir = "reports", params = list(file = file))
+# file_names = commandArgs(trailingOnly = TRUE)
+# 
+# if ("-a" %in% file_names){
+#   folder = file_names[2]
+#   file_names = Sys.glob(str_c("folder", "/*.csv"))
+#   
+#   if(length(file_names == 0)) {
+#     print ("No files found in the specified folder")
+#   }
+# } else {file_names = file_names}
+# 
+# if(length(file_names) == 0){
+#   print("Please provid list of .csv files.
+#          To specify all .csv files, use -a and a data directory as input")
+# }
+# for (file in file_names) {
+#   out_name <- str_split_fixed(string = file, pattern = "[/|.]", n =3) # split data file name into 3 parts by / | and .
+#   out_name <- str_c(out_name[2], ".html")
+#   render("surveys-report.Rmd", output_file = out_name, output_dir = "reports", params = list(file = file))
+# }
+
+# Rscript render-rmd.R -a data
+
+
+file_names <- commandArgs(trailingOnly = TRUE)
+
+if("-a" %in% file_names){
+  folder<-file_names[2]
+  file_names<-Sys.glob(str_c(folder, "/*.csv"))
+  
+  if(length(file_names) == 0){
+    print("No files found in the specified folder.")
+  }
+} else {file_names<-file_names}
+
+if(length(file_names) == 0){
+  print("Please provide a list of .csv files.
+        To specify all .csv files, use -a and a data directory as input.")
 }
 
-
+for(file in file_names)
+{
+  out_name <- str_split_fixed(string = file, pattern = "[/|.]", n = 3)
+  out_name<- str_c(out_name[2], '.html')
+  render("surveys-report.Rmd", output_file = out_name, output_dir = "reports", params = list(file = file))
+}
